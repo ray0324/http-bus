db9ceb7ff9eba8916e74e250deeaac530a4f4838
ray0324 <ray0324@foxmail.com>
2018-08-21 17:10:26 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,16 +2,13 @@ const HttpBus = require('../lib/http-bus');
 
 const httpbus = new HttpBus({
   port: 8001,
-  hosts: [
-    { link: '1', host: 'localhost', port: 9001 },
-    { link: '2', host: 'localhost', port: 9002 }
-  ],
-  prefix: '/reqxml',
-  staticRoot: 'E:\\hlcrm\\dist',
+  host: ['127.0.0.1:9001', '127.0.0.1:9002'],
+  prefix: /^\/api(\/.*)?$/,
+  rootDir: 'E:\\hlcrm\\dist',
   defaultIndex: 'index.html'
 });
 
 // 写日志
-httpbus.on('log', function(data) {
+httpbus.on('info', function(data) {
   console.log(data);
 });
--- test/server.js
+++ test/server.js
@@ -1,27 +1,29 @@
 const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 9001 });
-const wss2 = new WebSocket.Server({ port: 9002 });
-
 function noop() { }
 
+const wss1 = new WebSocket.Server({ port: 9001 });
+const wss2 = new WebSocket.Server({ port: 9002 });
+
 function heartbeat() {
-  console.log('Heartbeat:'+ Date.now());
-  console.log('wss.clients:' + wss.clients.size);
+  console.log('wss1.clients:' + wss1.clients.size);
+  console.log('wss2.clients:' + wss2.clients.size);
   this.isAlive = true;
 }
 
-wss.on('connection', function(ws) {
+wss1.on('connection', function(ws) {
   ws.isAlive = true;
   ws.on('pong', heartbeat);
   ws.on('message', function(data) {
-    console.log( 'wss:', data);
+    console.log( 'wss1msg:', data);
     const req = JSON.parse(data);
     ws.send(JSON.stringify({
       reqid: req.reqid,
       payload: {
-        time: Date.now(),
-        req: req.payload
+        delta: Date.now() - req.payload.reqtime,
+        req: req.payload,
+        resp: {
+          desc: '资讯服务'
+        }
       }
     }));
   });
@@ -31,13 +33,16 @@ wss2.on('connection', function(ws) {
   ws.isAlive = true;
   ws.on('pong', heartbeat);
   ws.on('message', function(data) {
-    console.log('wss2:', data);
+    console.log('wss2msg:', data);
     const req = JSON.parse(data);
     ws.send(JSON.stringify({
       reqid: req.reqid,
       payload: {
-        time: Date.now(),
-        req: req.payload
+        delta: Date.now() - req.payload.reqtime,
+        req: req.payload,
+        resp: {
+          desc: '行情服务'
+        }
       }
     }));
   });
@@ -45,7 +50,7 @@ wss2.on('connection', function(ws) {
 
 // 心跳
 setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
+  wss1.clients.forEach(function each(ws) {
     if (ws.isAlive === false) return ws.terminate();
     ws.isAlive = false;
     ws.ping(noop);
