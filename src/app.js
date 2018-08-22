8d741aad746cb2490399524d94a284a1e9e0c542
ray0324 <ray0324@foxmail.com>
2018-08-22 11:29:18 +0800

    update

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const HttpBus = require('../lib/http-bus');
 const httpbus = new HttpBus({
   port: 8001,
   host: ['127.0.0.1:9001', '127.0.0.1:9002'],
-  prefix: /^\/api(\/.*)?$/,
-  rootDir: 'E:\\hlcrm\\dist',
+  prefix: /^\/api([\/\?].*)?$/,
+  rootDir: 'E:\\WebRoot',
   defaultIndex: 'index.html'
 });
 
--- test/server.js
+++ test/server.js
@@ -5,8 +5,6 @@ const wss1 = new WebSocket.Server({ port: 9001 });
 const wss2 = new WebSocket.Server({ port: 9002 });
 
 function heartbeat() {
-  console.log('wss1.clients:' + wss1.clients.size);
-  console.log('wss2.clients:' + wss2.clients.size);
   this.isAlive = true;
 }
 
@@ -14,18 +12,12 @@ wss1.on('connection', function(ws) {
   ws.isAlive = true;
   ws.on('pong', heartbeat);
   ws.on('message', function(data) {
-    console.log( 'wss1msg:', data);
-    const req = JSON.parse(data);
-    ws.send(JSON.stringify({
-      reqid: req.reqid,
-      payload: {
-        delta: Date.now() - req.payload.reqtime,
-        req: req.payload,
-        resp: {
-          desc: '资讯服务'
-        }
-      }
-    }));
+    const { reqid, ...payload } = JSON.parse(data);
+    console.log('wss1msg:', payload);
+    const resp = {
+      desc: '行情服务'
+    };
+    ws.send(JSON.stringify({ reqid, payload: resp }));
   });
 });
 
@@ -33,23 +25,18 @@ wss2.on('connection', function(ws) {
   ws.isAlive = true;
   ws.on('pong', heartbeat);
   ws.on('message', function(data) {
-    console.log('wss2msg:', data);
-    const req = JSON.parse(data);
-    ws.send(JSON.stringify({
-      reqid: req.reqid,
-      payload: {
-        delta: Date.now() - req.payload.reqtime,
-        req: req.payload,
-        resp: {
-          desc: '行情服务'
-        }
-      }
-    }));
+    const { reqid, ...payload } = JSON.parse(data);
+    console.log('wss2msg:', payload);
+    const resp = {
+      desc: '资讯服务'
+    };
+    ws.send(JSON.stringify({ reqid, payload: resp }));
   });
 });
 
 // 心跳
 setInterval(function ping() {
+  console.log('wss1.clients:' + wss1.clients.size + ' wss2.clients:' + wss2.clients.size);
   wss1.clients.forEach(function each(ws) {
     if (ws.isAlive === false) return ws.terminate();
     ws.isAlive = false;
